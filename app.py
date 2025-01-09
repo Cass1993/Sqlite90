37300e5806cddcdae42439bd5f425805c388b4c5
Cass1993 <casianionascu@gmail.com>
2025-01-09 16:36:34 +0200

    Update database connection to use DATABASE_URL

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import psycopg2
+import os
+from urllib.parse import urlparse
+
+app = Flask(__name__)
+
+def get_db_connection():
+    result = urlparse(os.getenv('DATABASE_URL'))
+    conn = psycopg2.connect(
+        dbname=result.path[1:],
+        user=result.username,
+        password=result.password,
+        host=result.hostname,
+        port=result.port
+    )
+    return conn
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
